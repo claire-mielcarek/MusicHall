9f5656b702b5fef673e1c65740ea3a3281106e49
Arthurthefire <arthur.brehm@hotmail.fr>
2018-11-19 17:58:43 -0500

    Avancé sur la fonction recherche : Changement des critères de sélection Affichage de tous les groupes et musiciens sur la Bdd firebase Il faut maintenant faire fonctionner les critères dans la sélection de la recherche.

--- app/src/main/java/com/projet/musichall/Search/CriteresGroupe.java
+++ app/src/main/java/com/projet/musichall/Search/CriteresGroupe.java
@@ -1,47 +1,121 @@
 package com.projet.musichall.Search;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.projet.musichall.BaseActivity;
 import com.projet.musichall.R;
 
 import java.util.Date;
 
-public class CriteresGroupe extends BaseActivity {
-    RadioGroup instruments;
-    RadioGroup niveau;
+public class CriteresGroupe extends BaseActivity implements AdapterView.OnItemSelectedListener {
+
     RadioGroup motivation;
     TextView indicteur_instruments;
     TextView indicteur_niveau;
     TextView indicteur_motivation;
     TextView indicteur_localisation;
-    SeekBar valeurDistance;
     Button lancerRecherche;
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.search_criteres_musiciens);
+        setContentView(R.layout.search_criteres_groupe);
 
-        instruments = (RadioGroup)findViewById(R.id.instruments);
-        niveau = (RadioGroup)findViewById(R.id.niveau);
-        motivation = (RadioGroup)findViewById(R.id.motivation);
+        Spinner spinnerInstruments = findViewById(R.id.instruments);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.instruments, android.R.layout.simple_spinner_item );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerInstruments.setAdapter(adapter);
+        spinnerInstruments.setOnItemSelectedListener(this);
+
+        Spinner spinnerEcoute = findViewById(R.id.musiqueJouee);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.genres, android.R.layout.simple_spinner_item );
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerEcoute.setAdapter(adapter2);
+        spinnerEcoute.setOnItemSelectedListener(this);
+
+        Spinner spinnerNiveau = findViewById(R.id.niveau);
+        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this, R.array.niveaux, android.R.layout.simple_spinner_item );
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerNiveau.setAdapter(adapter4);
+        spinnerNiveau.setOnItemSelectedListener(this);
+
+        motivation = (RadioGroup) findViewById(R.id.motivation);
         indicteur_instruments = findViewById(R.id.indicateur_instrument);
         indicteur_niveau = findViewById(R.id.indicateur_niveau);
         indicteur_motivation = findViewById(R.id.indicateur_motivation);
         indicteur_localisation = findViewById(R.id.indicateur_localisation);
-        valeurDistance = findViewById(R.id.valeurDistance);
         lancerRecherche = findViewById(R.id.lancerRecherche);
+        /*
+        @Override
+        protected void onStart () {
+            super.onStart();
+            final TextView distance = findViewById(R.id.affichageDistance);
 
+            SeekBar seek = findViewById(R.id.valeurDistance);
+            seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    if (progress >= 0 && progress <= 20) {
+                        distance.setText("Débutant");
+                    } else if (progress >= 21 && progress <= 40) {
+                        distance.setText("Intermédiaire");
+                    } else if (progress >= 41 && progress <= 60) {
+                        distance.setText("Avancé");
+                    } else if (progress >= 61 && progress <= 80) {
+                        distance.setText("Expert");
+                    } else if (progress >= 81 && progress <= 100) {
+                        distance.setText("Maitre");
+                    }
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            });
+        }
+        */
+
+        lancerRecherche.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CriteresGroupe.this, RechercheActivity.class);
+                intent.putExtra("Search",0);
+                startActivity(intent);
+            }
+        });
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
+
+
--- app/src/main/java/com/projet/musichall/Search/RechercheActivity.java
+++ app/src/main/java/com/projet/musichall/Search/RechercheActivity.java
@@ -1,5 +1,8 @@
 package com.projet.musichall.Search;
 
+import android.content.Context;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -12,6 +15,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,28 +24,48 @@ import com.google.firebase.database.ValueEventListener;
 import com.projet.musichall.BaseActivity;
 import com.projet.musichall.R;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.function.Consumer;
 
 public class RechercheActivity extends BaseActivity {
 
     ListView listView;
     EditText editText;
-    ArrayList<String> listItems;
+    //Firebase variable
+    //Mettre plus tard pour la recherche auto
+    //private FirebaseAuth rech;
+    private DatabaseReference mDatabase;
+
+    //Array list
+    ArrayList<String> listItems = new ArrayList<>();
     ArrayAdapter<String> adapter;
 
     //items to show in listview
 
     String[] elementsRecherche;
+
+
+
     private String TAG;
+
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_listview);
 
+
+
+        mDatabase= FirebaseDatabase.getInstance().getReference();
         listView = (ListView)findViewById(R.id.listView);
         editText = (EditText)findViewById(R.id.recherche);
+        context = this;
+
+
 
         //Fonction pour initialiser la list de recherche
         init();
@@ -99,36 +123,72 @@ public class RechercheActivity extends BaseActivity {
         //Utiliser un listener propre à firebase pour récupérer les données dans ma classe
         // (Les lignes au dessus et en dessous sont liés)
         ///child puis getvalue en utilisant le read from database le 5
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef= database.getReference("message");
 
-        myRef.setValue("instrus");
+        // Uid, Nom, prénom, photo, date membre, ville, niveau, motivation
+        final int valeurRecherche = getIntent().getIntExtra("Search",0);
+        mDatabase.addValueEventListener(new ValueEventListener(){
 
-        // Lire la database
-        myRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                int counter=0;
+                DataSnapshot ref;
+
+                if(valeurRecherche==1) {
+                    DataSnapshot userSnapshot = dataSnapshot.child("Users");
+                    Iterator<DataSnapshot> it = userSnapshot.getChildren().iterator();
+                    elementsRecherche = new String[(int) userSnapshot.getChildrenCount()];
+                    while (it.hasNext()) {
+                        ref = it.next();
+                        elementsRecherche[counter] = String.valueOf(ref.child("prenom").getValue());
+                        elementsRecherche[counter] += " " + String.valueOf(ref.child("nom").getValue());
+                        elementsRecherche[counter] += " " + String.valueOf(ref.child("dateMembre").getValue());
+                        elementsRecherche[counter] += " " + String.valueOf(ref.child("ville").getValue());
+                        elementsRecherche[counter] += " " + String.valueOf(ref.child("motivation").getValue());
+                        elementsRecherche[counter] += " " + String.valueOf(ref.child("niveau").getValue());
+
+
+                        counter = counter + 1;
+                    }
+                }
+                else {
+                    DataSnapshot userSnapshot = dataSnapshot.child("Groupes");
+                    Iterator<DataSnapshot> it = userSnapshot.getChildren().iterator();
+                    elementsRecherche = new String[(int) userSnapshot.getChildrenCount()];
+                    while (it.hasNext()) {
+                        ref = it.next();
+                        elementsRecherche[counter] = String.valueOf(ref.child("nom").getValue());
+
+
+
+                        counter = counter + 1;
+                    }
+
+                }
+
+                listItems = new ArrayList<>(Arrays.asList(elementsRecherche));
+                //make an array of the objects according to a layout design
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(context,
+                        android.R.layout.simple_list_item_1, android.R.id.text1, elementsRecherche);
+                //set the adapter for listview
+                listView.setAdapter(adapter);
+
+
             }
 
             @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
 
-        elementsRecherche = new String[]{"Claire","Bastien","Arthur"};
 
-        listItems = new ArrayList<>(Arrays.asList(elementsRecherche));
-        //make an array of the objects according to a layout design
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, elementsRecherche);
-        //set the adapter for listview
-        listView.setAdapter(adapter);
+
+        //elementsRecherche = new String[]{"Claire","Bastien","Arthur"};
+
+
+
+
+
     }
 
     public void searchItem(String textToSearch){
